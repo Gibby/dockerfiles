4b1678c86452b021ccf33771f0cc8b8f6b6d2e9b
Drew Wells <drew.wells00@gmail.com>
2019-07-09 09:22:57 -0500

    vpn-slice broke more things than it fixed

--- openconnect/run.sh
+++ openconnect/run.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xe
 
 echo "Putting ssh keys in place"
 mkdir -p /root/.ssh
@@ -10,28 +10,28 @@ echo "Starting SSH"
 echo "UseDNS no" >> /etc/ssh/sshd_config
 /usr/sbin/sshd
 
-FILE="/keys/duo"
-if [! -a "$FILE" ]; then
-    echo "Key file is missing"
-    sleep 600
-    exit 1
-fi
+# FILE="/keys/duo"
+# if [! -a "$FILE" ]; then
+#     echo "Key file is missing"
+#     sleep 600
+#     exit 1
+# fi
 
-LOC=$(wc -l "$FILE")
+# LOC=$(wc -l "$FILE")
 
-if [ $LOC < 50 ]; then
-    echo "File is too small to pull from"
-    sleep 600
-    exit 1
-fi
-
-CODE=$(head -n1 "$FILE")
-# Remove first line of a file
-#tail -n +2 "$FILE" > "$FILE.tmp" && mv "$FILE.tmp" "$FILE"
+# if [ $LOC < 50 ]; then
+#     echo "File is too small to pull from"
+#     sleep 600
+#     exit 1
+# fi
+# CODE=$(head -n1 "$FILE")
+# # Remove first line of a file
+# #tail -n +2 "$FILE" > "$FILE.tmp" && mv "$FILE.tmp" "$FILE"
 
 echo "Starting openconnect!"
 #echo "Starting openconect"
-(echo "${PASSWORD}"; echo "push") | /usr/sbin/openconnect --verbose --disable-ipv6 --passwd-on-stdin ${OPTIONS} ${SERVER}
+#(echo "${PASSWORD}"; echo "push") | /usr/sbin/openconnect --verbose --disable-ipv6 --passwd-on-stdin ${OPTIONS[@]} -s "${SCRIPT[@]}" "${SERVER}"
+(echo "${PASSWORD}"; echo "push") | /usr/sbin/openconnect --verbose --disable-ipv6 --passwd-on-stdin ${OPTIONS[@]} "${SERVER}"
 echo "Openconnect stopped echo $@"
 sleep 60
 echo "Done sleeping"
